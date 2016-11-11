78363cf5747a815a016be38a8c42b102ea0feeef
Steve <retail.mbox@gmail.com>
2016-11-11 11:49:19 -0700

    add csv output creation

--- getinstances.py
+++ getinstances.py
@@ -4,11 +4,10 @@ import csv
 import pprint as pp
 
 # constants and flags
-# aws_profile = 'default'
-aws_profile = 'ew'
+aws_profile = 'default'
+# aws_profile = 'ew'
 aws_region = 'us-west-2'
 csvfilepath = 'instanceinfo.csv'
-csvheaders = ['img_id','architecture','img_type','description','platform','tags']
 
 # create the boto3 resource, via a session.  using this method to allow boto3 profile selection
 ec2_session = boto3.session.Session(profile_name=aws_profile)  # using session allows selection of aws profile
@@ -20,16 +19,6 @@ instances = ec2_resource.instances.all()
 # build a dict of interesting image attributes
 img_data_dict = support.compile_image_data(instances, ec2_resource)
 
-# todo: for each instance create a CSV row of interesting information
-# probably something like: instance_id, priv_dns, priv_ip, <assoc_image_info_fields>
+support.create_instance_csv_file(instances, img_data_dict, csvfilepath)
 
-
-# create a CSV to hold data
-# csvfile = open(csvfilepath, 'w')
-# csvwriter = csv.writer(csvfile)
-# csvwriter.writerow(csvheaders)
-# csvwriter.writerow((image, arch, img_type, desc, platform, tags))
-#
-# csvfile.flush()
-# csvfile.close()
 
--- getinstances_support.py
+++ getinstances_support.py
@@ -1,3 +1,6 @@
+import csv
+
+
 def get_unique_image_ids(instances):
 
     unique_images = set([])
@@ -7,9 +10,13 @@ def get_unique_image_ids(instances):
 
     return unique_images
 
+
 def compile_image_data(instances, ec2_resource):
     """
-    returns a dict of dicts, top level are the image id's, with attached image attributes
+    returns a dict of dicts, top level are the image id's, with attached image attributes.
+
+    Dict structure is: {'img_id_string1':{'img_attr11':'value11',...}, 'img_id_string2':{...}, ... }
+
     :param instances: aws ec2 instance iterable
     :param ec2_resource: boto3 ec2 resource object
     :return: dict-of-dicts containing image attributes
@@ -34,3 +41,57 @@ def compile_image_data(instances, ec2_resource):
         img_data_dict[img] = {'arch':arch, 'img_type': img_type, 'desc': desc, 'platform': platform, 'tags': tags}
 
     return img_data_dict
+
+
+def create_instance_csv_file(instances, img_data_dict, csv_file_path):
+
+    # todo: for each instance create a CSV row of interesting information
+    # probably something like: instance_id, priv_dns, priv_ip, <assoc_image_info_fields>
+
+    # create the CSV file at the path passed as param
+    f = open(csv_file_path, 'w')
+    csvwriter = csv.writer(f)
+
+    # write the header row
+    csvwriter.writerow(('instance_id', 'private_dns_name', 'private_ip', 'image_id', 'architecture', 'image_type',
+                        'description', 'platform', 'tags'))
+
+    for instance in instances:
+
+        # get the instance attributes we care about
+        inst_id = instance.id
+        priv_dns = instance.private_dns_name
+        priv_ip = instance.private_ip_address
+
+        # get the image attributes associated w/the instance, via the image_id
+        if instance.image_id not in img_data_dict:  # if problem getting img attrs for img_id, set values to indicate so
+            img_id = arch = img_type = desc = platform = tags = 'not found'
+        else:  #
+            img_id = instance.image_id
+            img_attr = img_data_dict [img_id]
+            arch = img_attr['arch']
+            img_type = img_attr['img_type']
+            desc = img_attr['desc']
+            platform = img_attr['platform']
+            tags = img_attr['tags']
+
+        row = [inst_id, priv_dns, priv_ip, img_id, arch, img_type, desc, platform, tags]
+
+        csvwriter.writerow(row)
+
+    f.flush()
+    f.close()
+
+
+
+
+
+
+# create a CSV to hold data
+csvfile = open(csvfilepath, 'w')
+csvwriter = csv.writer(csvfile)
+csvwriter.writerow(csvheaders)
+csvwriter.writerow((image, arch, img_type, desc, platform, tags))
+
+csvfile.flush()
+csvfile.close()
